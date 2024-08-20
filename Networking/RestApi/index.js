1355dd91fb02f79527033f2ded2c2d12b1a6919c
sidhanta <sidhantabasa007@gmail.com>
2024-08-20 17:51:05 +0530

    read the existing todo

--- Networking/RestApi/index.js
+++ Networking/RestApi/index.js
@@ -1,14 +1,42 @@
 import express from "express";
 
-const app= express();
+const app = express();
 
-app.all('/',(req,res) =>{
-    console.log("request=>",req);
-    console.log("response=>", res);
+app.all('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const PORT= 5111;
+const todos = [{
+    id: 1,
+    title: 'Buy milk',
+    completed: false
+},
+{
+    id: 2,
+    title: 'Buy coffee',
+    completed: true
+},
+
+
+]
+
+//read
+app.get('/todos', (req, res) => {
+    res.json(todos)
+
+
+})
+
+//create
+
+//update
+
+//delete
+
+
+
+
+const PORT = 5111;
 app.listen(PORT, () => {
-    console.log('server running at port',PORT);
+    console.log('server running at port', PORT);
 });
